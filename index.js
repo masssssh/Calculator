37bae86e7e75945dbc5299c460e1b18598bbfadb
masssssh <pykpykpyk000@yandex.ru>
2024-11-28 00:27:34 +0300

    operate function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let par1
+let par2
+let operator
+
 function sum (par1, par2) {
     return par1 + par2;
 }
@@ -17,3 +21,11 @@ function divide (par1, par2) {
     return par1 / par2;
 }
 console.log(divide(1,2));
+
+function operate (par1, par2, operator) {
+    if (operator === "+") return sum (par1, par2);
+    if (operator === "-") return subtract (par1, par2);
+    if (operator === "*") return multiply (par1, par2);
+    if (operator === "/") return divide (par1, par2);
+
+}
