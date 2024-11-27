0265a728e3d98c3af27f0e42b7f9f37d99ca0bed
masssssh <pykpykpyk000@yandex.ru>
2024-11-28 00:22:36 +0300

    main functions

---
+++ index.js
@@ -0,0 +1,19 @@
+function sum (par1, par2) {
+    return par1 + par2;
+}
+console.log(sum(1,2));
+
+function subtract (par1, par2) {
+    return par1 - par2;
+}
+console.log(subtract(1,2));
+
+function multiply (par1, par2) {
+    return par1 * par2;
+}
+console.log(multiply(1,2));
+
+function divide (par1, par2) {
+    return par1 / par2;
+}
+console.log(divide(1,2));
